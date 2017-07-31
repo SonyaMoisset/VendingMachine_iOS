df590f5bdeda47bbe30309e13160d80c3f1dc9f7
smoisset <smoisset@worldremit.com>
2017-07-31 20:12:27 +0100

    Upload cell file

---
+++ VendingMachine/VendingItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  VendingItemCell.swift
+//  VendingMachine
+//
+//  Created by Sonya Moisset on 30/07/2017.
+//  Copyright © 2017 Treehouse Island, Inc. All rights reserved.
+//
+
+import UIKit
+
+class VendingItemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var iconView: UIImageView!
+}
