343fa3413f513e30cd0f7e1a6faaf29b3c0fd92a
smoisset <smoisset@worldremit.com>
2017-08-09 07:20:12 +0100

    Update VendingMachine.swift

--- VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine.swift
@@ -147,6 +147,7 @@ class FoodVendingMachine: VendingMachine {
     }
     
     func deposit(_ amount: Double) {
+        amountDeposited += amount
     }
     
     func item(forSelection selection: VendingSelection) -> VendingItem? {
